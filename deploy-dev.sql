2cdf6ba2ed6f35b41c29ffb4bf470782efe449d7
Ramadevic <109077282+Ramadevic@users.noreply.github.com>
2025-07-16 04:14:49 -0400

    Update deploy-dev.sql

--- deploy-dev.sql
+++ deploy-dev.sql
@@ -1,33 +1,13 @@
-name: Deploy to Snowflake
+run: |
+  echo "🔍 Checking connection to Snowflake..."
 
-on:
-  push:
-    branches:
-      - main
-
-jobs:
-  deploy:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v3
-
-      - name: Install Snowflake CLI
-        uses: snowflakedb/snowflake-cli-action@v1.5
-        with:
-          cli-version: '3.6.0'  # You can switch to '3.5.0' if needed
-
-      - name: Run SQL using Snowflake CLI with direct connection flags
-        run: |
-          echo "📡 Connecting to Snowflake..."
-          snow sql \
-            --account "${{ secrets.SNOWSQL_ACCOUNT }}" \
-            --user "${{ secrets.SNOWSQL_USER }}" \
-            --password "${{ secrets.SNOWSQL_PWD }}" \
-            --role "${{ secrets.SNOWSQL_ROLE }}" \
-            --warehouse DEVOPS_WH \
-            --database DEVOPS_DB \
-            --schema COMMON \
-            --filename deploy-dev.sql \
-            --debug
+  snow sql \
+    --account "${{ secrets.SNOWSQL_ACCOUNT }}" \
+    --user "${{ secrets.SNOWSQL_USER }}" \
+    --password "${{ secrets.SNOWSQL_PWD }}" \
+    --role "${{ secrets.SNOWSQL_ROLE }}" \
+    --warehouse DEVOPS_WH \
+    --database DEVOPS_DB \
+    --schema COMMON \
+    --filename deploy-dev.sql \
+    --debug

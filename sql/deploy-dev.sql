036d5d1cfb997fb0f922af79a8023b1a748075d4
rama <chramadevi0922@gmail.com>
2025-07-28 12:44:11 -0400

    modified

--- sql/deploy-dev.sql
+++ sql/deploy-dev.sql
@@ -1,27 +1,2 @@
--- Set context for development environment
-USE ROLE ACCOUNTADMIN;
-USE WAREHOUSE DEVOPS_WH;
-USE DATABASE DEVOPS_DB;
-USE SCHEMA COMMON;
-
--- Log deployment start
-SELECT CURRENT_TIMESTAMP() AS deployment_start,
-       'DEV' AS environment,
-       CURRENT_USER() AS deployed_by;
-
--- Deploy Stage
-CALL COMMON.DEPLOY_STAGES();
-
--- Deploy Table
-CALL COMMON.DEPLOY_TABLES();
-
--- Deploy View
-CALL COMMON.DEPLOY_VIEWS();
-
--- Deploy Stored Procedure
-CALL COMMON.DEPLOY_STORED_PROCEDURES();
-
--- Log deployment completion
-SELECT CURRENT_TIMESTAMP() AS deployment_end,
-       'DEV' AS environment,
-       CURRENT_USER() AS deployed_by;
+EXECUTE IMMEDIATE FROM @SNOWFLAKE-POC/branches/main/Database/Schema/Tables/snowflake_customer_table.sql USING (ENV=>'DEV');
+ALTER GIT REPOSITORY FETCH;

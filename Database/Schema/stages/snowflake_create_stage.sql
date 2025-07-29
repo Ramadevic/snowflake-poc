bc845066f93adad2daff6e9adfe7f9a63cee7cff
Ramadevic <109077282+Ramadevic@users.noreply.github.com>
2025-07-29 02:11:45 -0400

    Update snowflake_create_stage.sql

--- Database/Schema/stages/snowflake_create_stage.sql
+++ Database/Schema/stages/snowflake_create_stage.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE STAGE my_stage
+CREATE OR REPLACE STAGE snowflake-poc
   FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY = '"')
   COMMENT = 'Stage for loading customer data';

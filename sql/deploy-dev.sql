8dd27aa0bec1eba176c5f38f1d7c0331b8d507c2
Ramadevic <109077282+Ramadevic@users.noreply.github.com>
2025-07-30 09:08:50 -0400

    Update deploy-dev.sql

--- sql/deploy-dev.sql
+++ sql/deploy-dev.sql
@@ -1,4 +1,4 @@
-snow git execute @@snowflake-poc/branches/main/Database/Schema/tables/snowflake_customer_table.sql" \
+snow git execute "@@snowflake-poc/branches/main/Database/Schema/tables/snowflake_customer_table.sql" \
     -D "environment='dev'"
 ALTER GIT REPOSITORY snowflake-poc FETCH;
 
